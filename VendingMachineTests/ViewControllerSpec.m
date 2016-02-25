835ab4306b43110afa9130d9887ef1288f2e0d35
keitzer <ogorek@usc.edu>
2016-02-25 16:49:28 -0500

    View Controller test file created

---
+++ VendingMachineTests/ViewControllerSpec.m
@@ -0,0 +1,16 @@
+//
+//  ViewControllerSpec.m
+//  VendingMachine
+//
+//  Created by Alex Ogorek on 2/25/16.
+//  Copyright © 2016 Kata. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "ViewController.h"
+#import <Kiwi.h>
+
+SPEC_BEGIN(ViewControllerSpec)
+
+
+SPEC_END
