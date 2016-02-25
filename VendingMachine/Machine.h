819a4969d6b0392c582a779c72a934a0172d715e
keitzer <ogorek@usc.edu>
2016-02-25 15:18:38 -0500

    Machine model class created

---
+++ VendingMachine/Machine.h
@@ -0,0 +1,13 @@
+//
+//  Machine.h
+//  VendingMachine
+//
+//  Created by Alex Ogorek on 2/25/16.
+//  Copyright © 2016 Kata. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Machine : NSObject
+
+@end
---
+++ VendingMachine/Machine.m
@@ -0,0 +1,13 @@
+//
+//  Machine.m
+//  VendingMachine
+//
+//  Created by Alex Ogorek on 2/25/16.
+//  Copyright © 2016 Kata. All rights reserved.
+//
+
+#import "Machine.h"
+
+@implementation Machine
+
+@end
