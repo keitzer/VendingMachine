bd0892a494a60bf56833d0995ad7700830837fd9
keitzer <ogorek@usc.edu>
2016-02-25 15:22:57 -0500

    machine should exist test failed

--- VendingMachineTests/MachineSpec.m
+++ VendingMachineTests/MachineSpec.m
@@ -8,8 +8,20 @@
 
 #import <XCTest/XCTest.h>
 #import <Kiwi.h>
+#import "Machine.h"
 
 SPEC_BEGIN(MachineSpec)
 
+describe(@"Machine", ^{
+	
+	__block Machine *vendingMachine;
+	
+	context(@"when first created", ^{
+		
+		it(@"should exist", ^{
+			[[vendingMachine shouldNot] beNil];
+		});
+	});
+});
 
 SPEC_END
